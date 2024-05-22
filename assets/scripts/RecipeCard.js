a3e60a722d8b7d60ea5dae31634031f7f15ce5a0
karinnamonzon <114766426+karinnamonzon@users.noreply.github.com>
2024-05-21 20:49:04 -0700

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,13 +8,13 @@ class RecipeCard extends HTMLElement {
 
 		// EXPOSE - START (All expose numbers start with A)
 		// A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
-		let shadowDOM = this.attachShadow({mode:'open'});
+		this.attachShadow({mode:'open'});
 		// A2. TODO - Create an <article> element - This will hold our markup once our data is set
-		let articleElement = document.createElement('article');
+		this.article = document.createElement('article');
 		// A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-		let styleElement = document.createElement('style');
+		this.styleElement = document.createElement('style');
 		// A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made (copy everything INSIDE the <style> tag>)
-		styleElement.innerText = ` 
+		this.styleElement.textContent = ` 
 		* {
 			font-family: sans-serif;
 			margin: 0;
@@ -91,8 +91,8 @@ class RecipeCard extends HTMLElement {
 			font-size: 12px;
 		  }`;
 		// A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-		shadowDOM.append(styleElement);
-		shadowDOM.append(articleElement);
+		this.shadowRoot.appendChild(styleElement);
+		this.shadowRoot.appendChild(articleElement);
 	}
 
 	/**
@@ -121,7 +121,7 @@ class RecipeCard extends HTMLElement {
 		if (!data) return;
 
 		// A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-		const shadowArticle = this.shadowRoot.querySelector("article");
+		let shadowArticle = this.shadowRoot.querySelector('article');
 		// A7. TODO - Set the contents of the <article> with the <article> template given in
 		//           cardTemplate.html and the data passed in (You should only have one <article>,
 		//           do not nest an <article> inside another <article>). You should use template
